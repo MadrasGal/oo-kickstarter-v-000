7bfd0f73e956f34b9b742d80a32ccadf0d39707b
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 02:48:05 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,13 +3,12 @@ class Backer
 attr_accessor :name, :project
 
 
-def initialize(name, project)
+def initialize(name)
   @name=name
-  project = Project.new
 end
 
 def back_project (project)
-  
+  project = Project.new
   @backed_projects << project
 end
 
