1689f060cc424724397dd4d666a976fdcbf5ae59
Purnima  Lakshmi Narayanan <purnima0912@hotmail.com>
2017-10-02 02:39:32 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,8 +7,9 @@ def initialize(name)
   @name=name
 end
 
-def back_project (:project)
-  @backed_projects << :project
+def back_project (project)
+  project = Project.new
+  @backed_projects << project
 end
 
 end
